c2e94606a684423633a14808877c0f4479b0c8f7
Joky8926 <262029232@qq.com>
2017-06-04 20:03:56 +0800

    Add CSGameObject

---
+++ Assets/Script/Logic/Util/CSGameObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CSGameObject {
+	public static void AddGameObject(GameObject goChild, GameObject goParent) {
+		Vector3 localPos = goChild.transform.localPosition;
+		goChild.transform.parent = goParent.transform;
+		goChild.transform.localScale = Vector3.one;
+		goChild.transform.localPosition = localPos;
+	}
+}
--- Assets/Script/View/Maze/SCRMazePanel.cs
+++ Assets/Script/View/Maze/SCRMazePanel.cs
@@ -9,23 +9,13 @@ public class SCRMazePanel : MonoBehaviour {
 
 	void Awake() {
 		preFactory = CSPrefabFactory.Instance();
-		CreateMazeGrid();
-		CreateMazePlayer();
+		AddChild();
 	}
 
-	private void CreateMazeGrid() {
+	private void AddChild() {
 		goMazeGrid = preFactory.GetMazeGrid();
-		Vector3 localPos = goMazeGrid.transform.localPosition;
-		goMazeGrid.transform.parent = this.transform;
-		goMazeGrid.transform.localScale = Vector3.one;
-		goMazeGrid.transform.localPosition = localPos;
-	}
-
-	private void CreateMazePlayer() {
 		goMazePlayer = preFactory.GetMazePlayer();
-		Vector3 localPos = goMazePlayer.transform.localPosition;
-		goMazePlayer.transform.parent = this.transform;
-		goMazePlayer.transform.localScale = Vector3.one;
-		goMazePlayer.transform.localPosition = localPos;
+		CSGameObject.AddGameObject(goMazeGrid, this.gameObject);
+		CSGameObject.AddGameObject(goMazePlayer, this.gameObject);
 	}
 }
